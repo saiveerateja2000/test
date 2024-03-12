c8378b019a9d2c3d77bd854f1dcd51c04309e87b
saiveerateja2000 <142569965+saiveerateja2000@users.noreply.github.com>
2024-03-12 21:35:50 +0530

    Create test_example.py

---
+++ test_example.py
@@ -0,0 +1,10 @@
+import example
+
+def test_add():
+    assert example.add(2, 3) == 5
+
+def test_subtract():
+    assert example.subtract(5, 3) == 2
+
+def test_multiply():
+    assert example.multiply(2, 3) == 6
